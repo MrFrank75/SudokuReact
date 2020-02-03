0b31588bebbf766c67e9f94b4b519356407a0045
Livio Francescucci <livio.francescucci@EF.com>
2020-02-03 13:46:59 +0100

    Generating a matrix of 9 numbers

--- src/SudokuMatrixGenerator.js
+++ src/SudokuMatrixGenerator.js
@@ -4,7 +4,28 @@ class SudokuMatrixGenerator extends React.Component{
 
     generateMatrix = async (event) => {
         event.preventDefault();
-        this.props.matrixGenerationHandler([[11,22,38],[44,55,66],[77,88,98],4,5,6,7,8,99])
+
+        var initialArray = [1,2,3,4,5,6,7,8,9];
+        var grid = [];
+        var maxElementsToPrefill = 9;
+
+        for (let index = 0; index < maxElementsToPrefill; index++) {
+            //pick an item contained in the array
+            var idxNextItemToPick = (Math.trunc(Math.random() * 10)) % initialArray.length;   
+            grid.push(initialArray[idxNextItemToPick]);
+            
+            //build a new smaller array
+            var newInitializationArray = [];
+            for (let index = 0; index < initialArray.length; index++) {
+                if (index!==idxNextItemToPick){
+                    newInitializationArray.push(initialArray[index]);
+                }
+            }
+            initialArray = newInitializationArray;
+        }
+        
+        console.log(grid);
+        this.props.matrixGenerationHandler([grid,[44,55,66],[77,88,98],4,5,6,7,8,99])
     }
 
     render(){
